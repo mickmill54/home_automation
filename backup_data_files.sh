eb4086c98bebc2d20294ec3ea22cfe9bae4bd29c
Mick Miller <mick@mickmill.com>
2022-10-21 03:46:40 +0000

    aded logging

--- backup_data_files.sh
+++ backup_data_files.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
+# Setup log file name with runtime date and time stamp
+TIMESTAMP=`date "+%Y-%m-%d-%H%M%S"`
+LOGFILE="/home/mick/logs/backup_logs-${TIMESTAMP}.txt"
 
 local_backup () {
   # Backup brak data-1 and data-2 files to local data-3 storage
-  rsync -auvzh --progress /data-1/ /data-3/data-1/
-  rsync -auvzh --progress /data-2/ /data-3/data-2/
+  rsync -auvzh --progress /data-1/ /data-3/data-1/ >> ${LOGFILE}
+  rsync -auvzh --progress /data-2/ /data-3/data-2/ >> ${LOGFILE}
 }
 
 remote_data-1_backup () {
 # Backup brak data-1 to zorak data-1
-rsync -auvzh --progress /data-1/ mick@10.1.1.30:/data-1/
+rsync -auvzh --progress /data-1/ mick@10.1.1.30:/data-1/ >> ${LOGFILE}
 }
 
 remote_data-2_backup () {
 # Backup brak data-2 to zorak data-2
-rsync -auvzh --progress /data-2/ mick@10.1.1.30:/data-2/
+rsync -auvzh --progress /data-2/ mick@10.1.1.30:/data-2/ >> ${LOGFILE}
 }
 
 
 main () {
-echo "===================================================================================================="
-echo "Starting backup jobs..."
-echo "===================================================================================================="
-local_backup
-remote_data-1_backup
-remote_data-1_backup
-echo "===================================================================================================="
-echo "End backup jobs"
-echo "===================================================================================================="
+echo "====================================================================================================" > ${LOGFILE}
+echo "Starting backup jobs -  `date +%Y-%m-%d-%H:%M:%S`" >> ${LOGFILE}
+echo "====================================================================================================" >> ${LOGFILE}
+time local_backup
+time remote_data-1_backup
+time remote_data-1_backup
+echo "====================================================================================================" >> ${LOGFILE}
+echo "End backup jobs  -  `date +%Y-%m-%d-%H:%M:%S`" >> ${LOGFILE}
+echo "====================================================================================================" >> ${LOGFILE}
 }
 
 time main
